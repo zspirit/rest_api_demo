6b43047f5af84d199e741a280d19c3f213a22392
Zakaria <zgafaoui@gmail.com>
2022-01-25 09:26:41 +0100

    update TU

--- web/tests/test_dummy.py
+++ web/tests/test_dummy.py
@@ -29,22 +29,3 @@ class TestUsersRegistration(unittest.TestCase):
             }
             self.assertEqual(response_json, expected_json)
 
-    def test_inscription(self):
-        request = {
-            "username": "demo",
-            "password": "demo",
-            "email": "demo@gmail.com"
-        }
-
-        with patch.object(src.database, "mongo", PyMongoMock()):
-            app = create_app("mongodb://localhost:27017/db").test_client()
-            response = app.post("/register", json=request)
-            self.assertEqual(response.status_code, 200)
-
-            # Validate the content
-            response_json = response.get_json()
-            expected_json = {
-                "status": 200,
-                "msg": "Registration successful, please check your mail to confirm your account",
-            }
-            self.assertEqual(response_json, expected_json)
